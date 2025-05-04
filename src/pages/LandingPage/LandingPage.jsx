3a36e1889b79a35b5a57ffba1bd7d53afddd8dc1
CarlTobias <113130276+CarlTobias@users.noreply.github.com>
2025-05-04 20:46:33 +0400

    initialization + testing

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <>
+    The airplane has landed! (get it? landing page? landed?)
+    </>
+  )
+}
+
+export default LandingPage
