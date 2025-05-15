f6deb8c85af094302520a1a51a554525e72cc4a6
raven101010 <114132446+raven101010@users.noreply.github.com>
2025-05-16 02:31:57 +0400

    help carl

---
+++ src/components/MyCar/MyCar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Flex } from "@chakra-ui/react";
+
+const MyCar = () => {
+  return (
+    <div>MyCar</div>
+  )
+}
+
+export default MyCar
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import "../../index.css";
 import { Flex } from "@chakra-ui/react";
 import HomeBar from "../../components/HomeBar/HomeBar";
 import MyForum from "../../components/MyForum/MyForum";
+import MyCar from "../../components/MyCar/MyCar";
 
 const HomePage = () => {
   return (
@@ -13,6 +14,7 @@ const HomePage = () => {
 
         {/* These will be navigated similar to how it is in the auth page with sign up and sign in */}
         {/* <MyCar /> */}
+        <MyCar />
         {/* <MyForum /> (default landing after login) */}
         <MyForum />
       </Flex>
