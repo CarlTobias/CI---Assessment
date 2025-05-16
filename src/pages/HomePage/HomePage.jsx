c7a0cad6ca73861d0a52d8799ef1b1c2cb8fd8c5
CarlTobias <113130276+CarlTobias@users.noreply.github.com>
2025-05-16 13:07:26 +0400

    merge update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -22,15 +22,7 @@ const HomePage = () => {
       <Flex minH={"100vh"} flexDirection={"column"}>
         <HomeBar />
 
-<<<<<<< HEAD
-        {/* These will be navigated similar to how it is in the auth page with sign up and sign in */}
-        {/* <MyCar /> */}
-        <MyCar />
-        {/* <MyForum /> (default landing after login) */}
-        <MyForum />
-=======
         {mode === "forum" ? <MyForum /> : <MyCar />}
->>>>>>> 0b67a9774c1a3917a1b1855c257c732c41d7567c
       </Flex>
     </>
   );
